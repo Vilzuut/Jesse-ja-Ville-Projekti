deb5c974bb7ee62564c86875e7363a0d87851e8e
unknown <jesse.mitronen@edu.keuda.fi>
2020-09-09 11:29:10 +0300

    okei mutsi

--- kirjautumislomake.js
+++ kirjautumislomake.js
@@ -1,1 +1,13 @@
-ajdajdioasjd
+function lahetys()
+{
+var empt = document.inputs["etunimi"]["sukunimi"].value;
+if (empt == "")
+{
+alert("Kirjoita nimesi");
+return false;
+}
+else
+{
+return true;
+}
+}
