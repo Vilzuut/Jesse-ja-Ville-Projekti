ec240377215cfe6e6c1a30618b01b4d3c4652d25
unknown <jesse.mitronen@edu.keuda.fi>
2020-09-17 09:19:28 +0300

    aqswdefrtgyuiop

---
+++ Projekti 2/laskin.js
@@ -0,0 +1,25 @@
+var = numero;
+var luku
+taulu =['+', '-', '/', '*', '%'];
+for(var i<0, i<numero.lenght, i++);
+{
+  for(var k=0; k<taulu.lenght, k++);
+}
+{
+  if(????=taulu[k])
+}
+{
+  indeksi = i
+}
+lisaa(luku)
+{
+  numero += luku;
+}
+
+
+function laske()
+{
+  numero = eval(numero);
+  numero.search('%');
+jono = numero.split('%')
+}
